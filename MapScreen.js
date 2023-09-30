9a97a2e2b8c0ec887c55cc4406942e42af9ac7a3
Abdullah Ahmad <abdullahaak06@gmail.com>
2023-09-30 16:06:01 +0500

    dump attempt at making zoomable map (commented)

--- MapScreen.js
+++ MapScreen.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { View, Text, Button, Image, StyleSheet } from "react-native";
+// import ZoomableImage from "./ZoomableImage";
+
+// import React from "react";
+// import { View, Text, Button, Image, StyleSheet } from "react-native";
+import { PanGestureHandler, State } from "react-native-gesture-handler";
 
 export default function Screen1({ navigation }) {
     return (
         <View>
             <Text>This is Askari 5 Map</Text>
+            {/* <div style={{ overflow: "auto" }}> */}
             <Image
                 source={require("./assets/img/map.jpeg")}
-                style={styles.storeImage}
+                style={styles.mapImage}
             ></Image>
+            {/* </div> */}
+            {/* <ZoomableImage /> */}
             <Button
                 title="Home"
                 onPress={() => navigation.navigate("Screen1")}
@@ -26,5 +34,6 @@ const styles = StyleSheet.create({
         width: 600,
         height: 600,
         display: "inline",
+        overflow: "auto",
     },
 });
